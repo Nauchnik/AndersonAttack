51e9882fb6fe8a875a32d41c8b10b2a258f6040f
Oleg Zaikin <zaikin.icc@gmail.com>
2016-10-20 19:26:49 +0800

    + creation of WUs

---
+++ boinc_a5_btf.cpp
@@ -0,0 +1,53 @@
+#ifdef _WIN32
+#include "boinc_win.h"
+#else
+#include <cstdio>
+#include <cctype>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <unistd.h>
+#endif
+
+#include <boinc_api.h>
+#include <fstream>
+#include <iostream>
+
+#define INPUT_FILENAME  "in"
+#define OUTPUT_FILENAME "out"
+
+unsigned long long RunKernel(unsigned long long c_stream, unsigned long long start, unsigned long long stop);
+int main()
+{
+	char buf[256];
+	int retval = boinc_init();
+	if (retval)
+	{
+		fprintf(stderr, "%s boinc_init returned %d\n",
+			boinc_msg_prefix(buf, sizeof(buf)), retval);
+		exit(retval);
+	}
+
+	std::ofstream outfile;
+
+	unsigned long long a, b, c;
+	{
+		std::string input_path;
+		boinc_resolve_filename_s (INPUT_FILENAME, input_path);
+		std::ifstream infile (input_path.c_str(), std::ios_base::in);
+
+		infile >> std::hex >> a >> b >> c;
+	}
+	//std::cout << std::endl << " " << a << " " << b  << " " << c << std::endl;
+	unsigned long long out = RunKernel(a, b, c);
+	{
+		std::string output_path;
+		boinc_resolve_filename_s (OUTPUT_FILENAME, output_path);
+		std::ofstream outfile (output_path.c_str(), std::ios_base::out);
+		outfile << std::hex << a << " " << out ;
+	}
+
+	boinc_finish(0);
+	return 0;
+}
---
+++ mkboinc_btf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#nvcc -c kernel.cu -O3 -D_FORCE_INLINES -dlink --ptxas-options="-v -dlcm=cg" -lcuda -lcudart -I/usr/local/cuda/samples/common/inc
+nvcc -c kernel.cu -O3 -D_FORCE_INLINES --ptxas-options="-v -dlcm=cg" -I/usr/local/cuda/samples/common/inc -o kernel.o
+g++ -c ./boinc_a5_btf.cpp -o boinc_a5_btf.o  -I/usr/local/include/boinc/ 
+g++ ./boinc_a5_btf.o ./kernel.o /usr/local/lib/libboinc_api.a /usr/local/lib/libboinc.a -o boinc_a5_btf -pthread -L/usr/local/cuda/lib64 -lcuda -lcudart
